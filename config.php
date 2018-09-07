341b441a8ef1a811aff5df857bb84c99cef4eeb1
wangtao <wangtao3@9fbank.cc>
2018-09-07 19:35:22 +0800

    upload code

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+
+define( "WB_AKEY" , '3187708105' );
+define( "WB_SKEY" , 'f5b59680c49a857811199a25daa2663b' );
+define( "WB_CALLBACK_URL" , 'http://www.bookask.com:8080/?a=login&b=callback&c=weibo');
